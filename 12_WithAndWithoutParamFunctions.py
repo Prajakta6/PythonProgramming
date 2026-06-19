43ac17489c23e82dbe3ba2eae7045b41c0c9ce80
Prajakta Shinde <prajaktashinde67@gmail.com>
2026-06-19 20:10:45 +0530

    Add files via upload

---
+++ 12_WithAndWithoutParamFunctions.py
@@ -0,0 +1,14 @@
+#With Parameters Function example below
+
+def getArea(height, width):
+    return height * width
+
+areaResult = getArea(10, 5)
+print(areaResult)
+
+#Without Parameters Function example below
+
+def func():
+    print("Hello Prajakta, You are a nice person!")
+
+func()
