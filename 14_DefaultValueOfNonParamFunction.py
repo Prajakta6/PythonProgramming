0be5257a27820eac58f99d12cd372e0ff287ec13
Prajakta Shinde <prajaktashinde67@gmail.com>
2026-06-19 20:23:08 +0530

    Add files via upload

---
+++ 14_DefaultValueOfNonParamFunction.py
@@ -0,0 +1,11 @@
+def display_welcome_message():
+    print("Ganpati Bappa Morya")
+
+# Calling the function and saving its hidden output
+result = display_welcome_message()
+
+print("The caught return value is:", result)
+
+#OUTPUT
+#Ganpati Bappa Morya
+#The caught return value is: None
