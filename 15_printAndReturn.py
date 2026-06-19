b27da74a89d40b5265c7c00fbb7073b37bef1454
Prajakta Shinde <prajaktashinde67@gmail.com>
2026-06-19 22:06:24 +0530

    Add files via upload

---
+++ 15_printAndReturn.py
@@ -0,0 +1,19 @@
+def add_and_print(a,b):
+    result = a + b
+    print(result)
+
+total = add_and_print(5,3)
+
+#OUTPUT 
+#8
+
+
+def add_and_return(a,b):
+    return a + b
+
+total = add_and_return(5,3)
+final_total = total * 10
+print(final_total)
+
+#OUTPUT 
+#80
