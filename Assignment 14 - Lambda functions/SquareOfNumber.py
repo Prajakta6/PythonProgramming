8f7c766fca2d9da15ed2463e092b4783aafb698f
Prajakta Shinde <prajaktashinde67@gmail.com>
2026-07-02 16:43:03 +0530

    Implemented lambda function to calculate square of a number

--- Assignment 14 - Lambda functions/SquareOfNumber.py
+++ Assignment 14 - Lambda functions/SquareOfNumber.py
@@ -1,1 +1,7 @@
 # Write a lambda function which accepts one number and returns square of that number.
+
+SquareOfNumber = lambda No : No * No
+
+Ret = SquareOfNumber(10)
+
+print("Square Of Number is : ",Ret)
