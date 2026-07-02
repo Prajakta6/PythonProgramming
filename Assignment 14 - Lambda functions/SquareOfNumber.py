ea3550ec5c66f8ea19c56b6f4de483a6114476da
Prajakta Shinde <prajaktashinde67@gmail.com>
2026-07-02 17:40:32 +0530

    Add files via upload

--- Assignment 14 - Lambda functions/SquareOfNumber.py
+++ Assignment 14 - Lambda functions/SquareOfNumber.py
@@ -7,3 +7,7 @@ number = int(input("Enter a number : "))
 Ret = SquareOfNumber(number)
 
 print("Square of number", number , "is : ", Ret)
+
+# OUTPUT
+# Enter a number : 3
+# Square of number 3 is :  9
