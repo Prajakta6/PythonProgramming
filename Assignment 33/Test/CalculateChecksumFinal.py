430fae2f701014429bbfbf6f072f4720d58cd3df
Prajakta Shinde <prajaktashinde67@gmail.com>
2026-07-30 21:55:08 +0530

    Add CalculateChecksum function to compute file checksum
    This script calculates the MD5 checksum of a file and prints the result.

---
+++ Assignment 33/Test/CalculateChecksumFinal.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import hashlib
+
+def CalculateChecksum(FileName):
+    
+    fObj = open(FileName, "rb") #rb is binary I/O
+    
+    hObj = hashlib.md5() #hashlib is a module and md5 is a algorithm
+
+    Buffer = fObj.read(1024) #Reading 1024 bytes - 1 kb
+
+    while(len(Buffer) > 0):
+        hObj.update(Buffer)
+        Buffer = fObj.read(1024) #Again reading next 1024 bytes. At the end Buffer will be 0 so loop will end
+
+    fObj.close()
+
+    return hObj.hexdigest() #calculating checksum of whole file data
+
+
+def main():
+    Ret = CalculateChecksum("DemoX.txt")
+    print("Checksum of file is : ",Ret)
+
+if __name__ == "__main__":
+    main()
+
+#OUTPUT
+# Checksum of file is :  1a88f20756104dd343405e22464dac42
+# This is 32 character lower case hex string checksum
